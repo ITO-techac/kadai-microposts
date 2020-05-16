31137c4c6fec21d5e2618d1c73008a684e6f7b7e
b.itou <bebessei0401@gmail.com>
2020-05-16 13:40:15 +0000

    submit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,14 @@ class User < ApplicationRecord
     has_secure_password
     
     has_many :microposts
+    
     has_many :relationships
     has_many :followings, through: :relationships, source: :follow
     has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
     has_many :followers, through: :reverses_of_relationship, source: :user
+    
+    has_many :favorites
+    has_many :fav_microposts, through: :favorites, source: :micropost
     
     def follow(other_user)
         unless self == other_user
@@ -23,8 +27,21 @@ class User < ApplicationRecord
         relationship.destroy if relationship
     end
     
+    def favorite(micropost)
+        favorites.find_or_create_by(micropost_id: micropost.id)
+    end
+    
+    def unfavorite(micropost)
+        favorite = favorites.find_by(micropost_id: micropost.id)
+        favorite.destroy if favorite
+    end
+    
     def following?(other_user)
         self.followings.include?(other_user)
+    end
+    
+    def favorite?(micropost)
+        self.fav_microposts.include?(micropost)
     end
     
     def feed_microposts
